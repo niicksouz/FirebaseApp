cb087558d4e5bc4d3f02e356f7a28b1401a72827
monick.s.g2 <monick.s.g2@gmail.com>
2025-09-29 00:05:10 +0000

    Semana 6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,60 +1,37 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
-    id("dev.flutter.flutter-gradle-plugin")
+    // START: FlutterFire Configuration
     id("com.google.gms.google-services")
-}
-
-def localProperties = new Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localPropertiesFile.inputStream().use { stream ->
-        localProperties.load(stream)
-    }
-}
-
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode")
-if (flutterVersionCode == null) {
-    throw new GradleException("Flutter version code not found. Please run 'flutter pub get' in your project.")
-}
-
-val flutterVersionName = localProperties.getProperty("flutter.versionName")
-if (flutterVersionName == null) {
-    throw new GradleException("Flutter version name not found. Please run 'flutter pub get' in your project.")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.myapp"
-    compileSdk = 34
-    ndkVersion = "25.1.8937393"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    sourceSets {
-        getByName("main") {
-            java.srcDirs("src/main/kotlin")
-        }
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.myapp"
-        // Formats the version name specified in pubspec.yaml into something readable by Android.
-        // For example, "1.0.0" becomes "1.0.0".
-        versionName = flutterVersionName
-        // Formats the version code specified in pubspec.yaml into something readable by Android.
-        // For example, "1" becomes "1".
-        versionCode = flutterVersionCode.toInt()
-        minSdk = 21
-        targetSdk = 34
-        multiDexEnabled = true
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
@@ -68,4 +45,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
